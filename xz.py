6caebbdb031d6f19fcd3e3f3d6e8800afe0d4095
ashishsharma8800 <37451999+ashishsharma8800@users.noreply.github.com>
2022-04-18 23:45:15 +0530

    Update xz.py

--- xz.py
+++ xz.py
@@ -2,12 +2,16 @@
 
 import cv2
 import numpy as np from PIL import Image import os
+
 def darw_boundary(img,classifier,scalefactor,minNeighbors,color,text,clf): gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 features = classifier.detectMultiScale(gray_image,scalefactor,minNeighbors)
 coords = []
+
 for (x,y,w,h) in features: cv2.rectangle(img,(x,y),(x+w,y+h),color,2) id,pred = clf.predict(gray_image[y:y+h,x:x+w]) confidence = int(100*(1-pred/300))
+
 if confidence > 75: if id==1:
 cv2.putText(img,"vini",(x,y-5), cv2.FONT_HERSHEY_SIMPLEX,0.8,color,1,cv2.LINE_AA)
+
 if id==2: cv2.putText(img,"rex",(x,y-5),
 cv2.FONT_HERSHEY_SIMPLEX,0.8,color,1,cv2.LINE_AA) else:
 cv2.putText(img,"UNKNOWN",(x,y-5),
